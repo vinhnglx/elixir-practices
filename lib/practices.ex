4cf731d0f245dcad4b0bfe252eb8b1cdfb439dc4
Vincent <vinh.nglx@gmail.com>
2017-06-03 20:04:24 +0800

    3 katas - 03/06/2017

---
+++ lib/practices.ex
@@ -0,0 +1,83 @@
+defmodule Practices do
+  @moduledoc """
+  Documentation for Practices.
+  """
+
+  @doc """
+    Count no of duplicates
+
+    ## Example
+
+        iex > Practices.count_duplicates("abABcdef")
+        2 # a,b
+        iex > Practices.count_duplicates("impossible")
+        2 # i,s
+  """
+  def count_duplicates(str) do
+    # NOTE: _ means this value is not be used
+    str
+      |> String.downcase()
+      |> String.split("", trim: true)
+      |> Enum.group_by(fn(x) -> x end)
+      |> Enum.count(fn({_key, value}) -> value |> Enum.count() > 1 end)
+  end
+
+  @doc """
+    String repeat
+
+    ## Example
+
+      iex > Practices.string_repeat(2, "Hello")
+      HelloHello
+      iex > Practices.string_repeat(0, "Vb")
+      ""
+  """
+  def string_repeat(number, str) do
+    if (number === 0 || number < 0) do
+      ""
+    else
+      if number > 0 do
+        str |> String.duplicate(number)
+      end
+    end
+  end
+
+  @doc """
+    Opposite number
+
+    ## Example
+
+      iex > Practices.opposite_num(2)
+      -2
+      iex > Practices.opposite_num(-2)
+      2
+  """
+  def opposite_num(number) do
+    number * -1
+  end
+
+  @doc """
+    Split String to pair of two chars
+
+
+    ## Example
+
+      iex > Practices.split_string_pair("abc")
+      ["ab", "c_"]
+      iex > Practices.split_string_pair("def12")
+      ["de", "f1", "2_"]
+      iex > Practices.split_string_pair("abcdef")
+      ["ab", "cd", "ef"]
+  """
+  def split_string_pair(str) do
+    str_list = str |> String.downcase |> String.codepoints
+
+    str_length = str |> String.length
+
+    if str_length |> rem(2) === 0 do
+      str_list |> Enum.chunk(2) |> Enum.map(fn(x) -> x |> Enum.join() end)
+    else
+      str_list |> List.insert_at(-1, "_") |> Enum.chunk(2) |> Enum.map(fn(x) -> x |> Enum.join() end)
+    end
+  end
+end
