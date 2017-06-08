697f1d03155b46390c4d28fdedf55419a400f94e
Vincent <vinh.nglx@gmail.com>
2017-06-08 17:58:52 +0800

    Test for count_duplicates kata

--- test/practices_test.exs
+++ test/practices_test.exs
@@ -2,7 +2,11 @@ defmodule PracticesTest do
   use ExUnit.Case
   doctest Practices
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "count no of duplicates in a word" do
+    {:ok, result} = Practices.count_duplicates({:string, "helloworld"})
+    assert result == 2
+
+    {:error, error} = Practices.count_duplicates(1111)
+    assert error == {:not_a_string, 1111}
   end
 end
