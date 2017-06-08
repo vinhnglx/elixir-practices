94c26e0b7ca2a8cb5c5e34d06cd9f9e7ff4cb421
Vincent <vinh.nglx@gmail.com>
2017-06-08 18:20:16 +0800

    Test for string_repeat kata

--- test/practices_test.exs
+++ test/practices_test.exs
@@ -9,4 +9,12 @@ defmodule PracticesTest do
     {:error, error} = Practices.count_duplicates(1111)
     assert error == {:not_a_string, 1111}
   end
+
+  test "repeat string base on the number" do
+    empty_result = Practices.string_repeat(0, "God")
+    assert empty_result == ""
+
+    result = Practices.string_repeat(3, "God")
+    assert result == "GodGodGod"
+  end
 end
